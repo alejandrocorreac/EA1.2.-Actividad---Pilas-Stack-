9f3f4eb717583066f273a8cda3460ec5eaeda70f
stevencadavid639-SR71 <stevencadavid639@gmail.com>
2026-03-05 20:59:29 -0500

    Commit Stiven 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
-
+import java.util.Scanner;
 
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Pila pilaAcciones = new Pila(100);
+        Pila pilaRedo = new Pila(100);
+
+        int opcion;
 
 
 
